2d96b18557f522fa013dede28521ed75043012fc
pifopi <dottel.gael@gmail.com>
2025-04-06 23:54:33 +0200

    convenience changes

---
+++ build.cmd
@@ -0,0 +1,10 @@
+pushd %~dp0
+cd SerialPrograms
+mkdir bin
+cd bin
+cmake .. -DCMAKE_PREFIX_PATH=C:\Qt\6.9.0\msvc2022_64\
+cmake --build . --config RelWithDebInfo
+cmake --build . --config Debug
+robocopy ..\..\..\Packages\SerialPrograms\Resources\    Resources\ /s
+robocopy ..\..\..\CommandLineTests\                     . /s
+popd
