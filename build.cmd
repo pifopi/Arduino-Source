b82d81990c6d031f2a7224c8cabb7cb7352c23b9
pifopi <dottel.gael@gmail.com>
2025-05-27 01:22:16 +0200

    temp changes for vcpkg

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@ pushd %~dp0
 cd SerialPrograms
 mkdir bin
 cd bin
-cmake .. -DCMAKE_PREFIX_PATH=C:\Qt\6.9.0\msvc2022_64\
+cmake .. -DCMAKE_PREFIX_PATH=C:\Qt\6.9.0\msvc2022_64\ -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake"
 cmake --build . --config RelWithDebInfo
 cmake --build . --config Debug
 robocopy ..\..\..\Packages\SerialPrograms\Resources\    Resources\ /s
