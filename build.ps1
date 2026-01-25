0041a78dc7d2d0b4187085e42351b206a1bf2d48
pifopi <dottel.gael@gmail.com>
2026-01-25 23:43:15 +0100

    convenience change

---
+++ build.ps1
@@ -0,0 +1,71 @@
+param
+(
+    [ValidateSet("MSVC", "CLANG")]
+    [string]$Compiler = "MSVC",
+
+    [ValidateSet("NONE", "VCPKG", "CONAN")]
+    [string]$PackageManager = "NONE"
+)
+
+enum Compiler
+{
+    MSVC
+    CLANG
+}
+$CompilerAsEnum = [Compiler]::$Compiler
+switch ($CompilerAsEnum)
+{
+    MSVC
+    {
+        $cmakeCompilerAdditionalParam = ""
+    }
+    CLANG
+    {
+        $cmakeCompilerAdditionalParam = "-T ClangCL"
+    }
+}
+
+enum PackageManager
+{
+    NONE
+    VCPKG
+    CONAN
+}
+$PackageManagerAsEnum = [PackageManager]::$PackageManager
+switch ($PackageManagerAsEnum)
+{
+    NONE
+    {
+        $cmakePackageManagerAdditionalParam = ""
+    }
+    VCPKG
+    {
+        $cmakePackageManagerAdditionalParam = "-DCMAKE_TOOLCHAIN_FILE=$PSScriptRoot\vcpkg\scripts\buildsystems\vcpkg.cmake"
+    }
+    CONAN
+    {
+        $cmakePackageManagerAdditionalParam = "-DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake"
+    }
+}
+
+robocopy ..\Packages Packages /s /Purge
+Push-Location $PSScriptRoot
+Set-Location SerialPrograms
+$binDirectory = "bin\" + [PackageManager]::$PackageManager + "\" + [Compiler]::$Compiler
+Write-Output "Building in $binDirectory"
+if (-not(Test-Path $binDirectory -PathType Container))
+{
+    New-Item -path $binDirectory -ItemType Directory
+}
+Set-Location $binDirectory
+
+switch ($PackageManagerAsEnum)
+{
+    CONAN
+    {
+        conan install ..\..\.. --output-folder=. --build=missing -s "&:build_type=RelWithDebInfo" -s build_type=Release -s compiler.cppstd=23
+    }
+}
+cmake ..\..\.. $cmakeCompilerAdditionalParam $cmakePackageManagerAdditionalParam
+cmake --build . --config RelWithDebInfo
+Pop-Location
